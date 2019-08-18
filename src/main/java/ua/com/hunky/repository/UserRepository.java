c4f34ba7d976eea60bc088120dd8d31dd18ee569
Hunkyua <opanasyuk.valentin@gmail.com>
2019-08-18 10:46:23 +0300

    remove postgresql.jar

--- src/main/java/ua/com/hunky/repository/UserRepository.java
+++ src/main/java/ua/com/hunky/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package ua.com.hunky.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.com.hunky.model.User;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
