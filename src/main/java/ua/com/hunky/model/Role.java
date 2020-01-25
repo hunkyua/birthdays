0129332836ea981237fb3bbe2d8f725df5ee70ac
Hunkyua <opanasyuk.valentin@gmail.com>
2020-01-25 13:39:19 +0200

    Refactoring Role for easier reading and code reduction

--- src/main/java/ua/com/hunky/model/Role.java
+++ src/main/java/ua/com/hunky/model/Role.java
@@ -3,8 +3,8 @@ package ua.com.hunky.model;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    UNKNOWN, ADMIN, USER;
 
+    UNKNOWN, ADMIN, USER;
 
     @Override
     public String getAuthority() {
