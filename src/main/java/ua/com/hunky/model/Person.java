1752422826c97ccf2618534f71622d7b75b68892
Hunkyua <opanasyuk.valentin@gmail.com>
2020-01-25 13:39:38 +0200

    Refactoring Person for easier reading and code reduction

--- src/main/java/ua/com/hunky/model/Person.java
+++ src/main/java/ua/com/hunky/model/Person.java
@@ -15,18 +15,25 @@ import java.sql.Date;
 @Entity
 @Table(name = "persons")
 public class Person {
+
     private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     @Column(name = "name")
     private String name;
+
     @Column(name = "surname")
     private String surname;
+
     @Column(name = "email")
     private String email;
+
     @Column(name = "dateOfBirth")
     private Date dateOfBirth;
+
     @Column(name = "userId")
     private Long userID;
 
