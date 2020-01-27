0bbdf27cacb355899e0440d946799874fc1a7b36
Hunkyua <opanasyuk.valentin@gmail.com>
2020-01-27 17:57:34 +0200

    Refactoring ExcelPersonExportAndImport for easier reading and code reduction

--- src/main/java/ua/com/hunky/service/ExcelPersonExportAndImport.java
+++ src/main/java/ua/com/hunky/service/ExcelPersonExportAndImport.java
@@ -3,6 +3,7 @@ package ua.com.hunky.service;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
 import org.springframework.web.servlet.view.document.AbstractXlsView;
+import ua.com.hunky.controller.PersonController;
 import ua.com.hunky.model.Person;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,29 +20,36 @@ public class ExcelPersonExportAndImport extends AbstractXlsView {
                                       HttpServletResponse response) {
 
         response.setHeader("Content-Disposition", "attachment;filename=\"person.xls\"");
-        List<Person> personList = (List<Person>) model.get("personList");
+
+        List<Person> persons = (List<Person>) model.get("personList");
+
         Sheet sheet = workbook.createSheet("Persons");
-        Row header = sheet.createRow(0);
-        CellStyle cellStyle = workbook.createCellStyle();
-        CreationHelper createHelper = workbook.getCreationHelper();
-        cellStyle.setDataFormat(createHelper.createDataFormat().getFormat("m/d/yy"));
-        header.createCell(0).setCellValue("ID");
-        header.createCell(1).setCellValue("Name");
-        header.createCell(2).setCellValue("Surname");
-        header.createCell(3).setCellValue("Email");
-        header.createCell(4).setCellValue("DateOfBirth");
+        setHeader(sheet);
 
-        int rowNum = 1;
-        for (Person person : personList) {
-            Row row = sheet.createRow(rowNum++);
-            row.createCell(0).setCellValue(person.getId());
-            row.createCell(1).setCellValue(person.getName());
-            row.createCell(2).setCellValue(person.getSurname());
-            row.createCell(3).setCellValue(person.getEmail());
-            Cell cell = row.createCell(4);
-            cell.setCellStyle(cellStyle);
-            cell.setCellValue(person.getDateOfBirth());
+        int number = 0;
+        for (Person person : persons) {
+            setRow(sheet, ++number, person);
+        }
+    }
 
+    private void setRow(Sheet sheet, int number, Person person) {
+        createRow(sheet, number,
+                String.valueOf(person.getId()),
+                person.getName(),
+                person.getSurname(),
+                person.getEmail(),
+                PersonController.DATE.format(person.getDateOfBirth()));
+    }
+
+    private void setHeader(Sheet sheet) {
+
+        createRow(sheet, 0, "ID", "Name", "Surname", "Email", "DateOfBirth");
+    }
+
+    private void createRow(Sheet sheet, int row, String... data) {
+        Row header = sheet.createRow(row);
+        for (int i = 0; i < data.length; i++) {
+            header.createCell(i).setCellValue(data[i]);
         }
     }
 
