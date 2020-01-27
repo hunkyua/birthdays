a7cdfa16d84139ff96ab92ab93e06003b1997244
Hunkyua <opanasyuk.valentin@gmail.com>
2020-01-27 16:31:49 +0200

    Refactoring UserService for easier reading and code reduction

--- src/main/java/ua/com/hunky/service/UserService.java
+++ src/main/java/ua/com/hunky/service/UserService.java
@@ -15,59 +15,63 @@ import java.util.UUID;
 
 @Service
 public class UserService implements UserDetailsService {
-    @Autowired
-    private UserRepo userRepo;
 
     @Autowired
-    MailSender mailSender;
+    private UserRepo users;
+
+    @Autowired
+    MailSender mail;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepo.findByUsername(username);
+        return users.findByUsername(username);
     }
 
     public boolean addUser(User user) {
-        User userFromDb = userRepo.findByUsername(user.getUsername());
+        User exist = users.findByUsername(user.getUsername());
 
-        if (userFromDb != null) {
+        if (exist != null) {
             return false;
         }
 
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
         user.setActivationCode(UUID.randomUUID().toString());
-        userRepo.save(user);
+        users.save(user);
 
         if (!StringUtils.isEmpty(user.getEmail())) {
-            String message = String.format(
-
-            "Hello, %s, \n" +
-            "Thanks for joining BirthdaysApp! Please confirm your email address and activate your account by clicking this link: \n\n" +
-            "http://localhost:8080/activate/%s \n\n" +
-            "If you have any trouble with your account, you can always email us at birthdaysapp.ua@gmail.com.\n" +
-            "Regards, \n" +
-            "The BirthdaysApp team. \n" +
-            "If you didn't register for BirthdaysApp, please ignore this message."  ,
-
-                    user.getUsername(),
-                    user.getActivationCode()
-            );
-            mailSender.send(user.getEmail(), "Activation code", message);
+            sendEmail(user);
         }
 
         return true;
     }
 
+    private void sendEmail(User user) {
+        String message = String.format(
+
+        "Hello, %s, \n" +
+        "Thanks for joining BirthdaysApp! Please confirm your email address and activate your account by clicking this link: \n\n" +
+        "http://localhost:8080/activate/%s \n\n" +
+        "If you have any trouble with your account, you can always email us at birthdaysapp.ua@gmail.com.\n" +
+        "Regards, \n" +
+        "The BirthdaysApp team. \n" +
+        "If you didn't register for BirthdaysApp, please ignore this message."  ,
+
+                user.getUsername(),
+                user.getActivationCode()
+        );
+        mail.send(user.getEmail(), "Activation code", message);
+    }
+
     public boolean activateUser(String code) {
-        User user = userRepo.findByActivationCode(code);
+        User user = users.findByActivationCode(code);
 
         if (user == null) {
             return false;
         }
 
         user.setActivationCode(null);
-
-        userRepo.save(user);
+        users.save(user);
 
         return true;
     }
